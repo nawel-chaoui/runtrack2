7c334ba00a9ed627f9f2539005e5da1ba01d97c5
Aka Nawelfe <nawel.chaoui@laplateforme.io>
2023-05-22 11:21:46 +0200

    ajout travaux

---
+++ job01/index.php
@@ -0,0 +1,36 @@
+<?php
+// Création de la variable str et affichage de son contenu
+$str = "LaPlateforme";
+echo $str . "<br>";
+
+// Création des variables str2 et str3
+$str2 = "Vive";
+$str3 = "!";
+
+// Affichage de la concaténation des variables
+echo $str2 . " " . $str . " " . $str3 . "<br>";
+
+// Création de la variable val et affectation de la valeur 6
+$val = 6;
+
+// Affichage du contenu de la variable val
+echo $val . " ";
+
+// Ajout de 4 à la variable val
+$val += 4;
+
+// Affichage du nouveau contenu de la variable val
+echo $val . " ";
+
+$myBool = true;
+
+// Affichage du contenu de la variable myBool
+var_dump($myBool);
+
+// Affectation de la valeur false à la variable myBool
+$myBool = false;
+
+// Affichage du nouveau contenu de la variable myBool
+var_dump($myBool);
+?>
+ 
---
+++ job02/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Affichage de la première phrase "Hello LaPlateforme!" avec un commentaire en ligne
+echo "Hello LaPlateforme!" . "<br>"; // Commentaire en ligne
+
+/*
+Affichage de la deuxième phrase "Hello LaPlateforme!" avec un commentaire en bloc
+Ce commentaire s'étend sur plusieurs lignes.
+*/
+echo "Hello LaPlateforme!"; /* Commentaire en bloc */
+
+?>
